2c36ce9c101876369c6d36dc051582770a315485
shrirampiyer <75976818+shrirampiyer@users.noreply.github.com>
2024-09-20 12:05:27 +0530

    first commit

---
+++ archieval_trigger.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+du -sh /Users/Shriram/Desktop/bluebolt_poc >> run_report.txt
+
+./archieve.sh >> run_report.txt
+
+du -sh /Users/Shriram/Desktop/bluebolt_poc >> run_report.txt
+
---
+++ archieve.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TARGET_PATH="/Users/Shriram/Desktop/bluebolt_poc"
+
+CURRENT_DATE=$(date +%s)
+
+ONE_MONTH_AGO=$(date -v-1m +%s)
+SIX_MONTHS_AGO=$(date -v-6m +%s)
+
+for file in "$TARGET_PATH"/*; do
+    if [ -f "$file" ]; then
+        FILE_MOD_DATE=$(stat -f %m "$file")
+
+        if [ "$FILE_MOD_DATE" -le "$SIX_MONTHS_AGO" ]; then
+            echo "Deleting: $file (older than 6 months)"
+            rm "$file"
+        elif [ "$FILE_MOD_DATE" -le "$ONE_MONTH_AGO" ]; then
+            echo "Compressing: $file (older than 1 month but less than 6 months)"
+            gzip "$file"
+        else
+            echo "File is less than 1 month old: $file (no action needed)"
+        fi
+    fi
+done
+
+echo "File archival process completed."
